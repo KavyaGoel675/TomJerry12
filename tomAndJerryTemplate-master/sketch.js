4fdfbe56cd6f70bb88880abdc8da1aa1f7061556
KavyaGoel675 <78598510+KavyaGoel675@users.noreply.github.com>
2021-04-06 19:45:08 +0530

    Add files via upload

---
+++ tomAndJerryTemplate-master/sketch.js
@@ -0,0 +1,61 @@
+var canvas,bg;
+var together;
+var tom, tomImg1,tomImg2, tomImg3;
+var jerry, jerryImg1,jerryImg2, jerryImg3;
+
+function preload() {
+    bg = loadImage("images/garden.png");
+    tomImg1= loadImage("images/cat1.png");
+    tomImg2=loadAnimation("images/cat2.png","images/cat3.png");
+    tomImg3= loadImage("images/cat4.png");
+    jerryImg1=loadImage("images/mouse1.png");
+    jerryImg2= loadAnimation("images/mouse2.png","images/mouse3.png");
+    jerryImg3=loadImage("images/mouse4.png");
+
+}
+
+function setup(){
+    canvas = createCanvas(1000,800);
+
+    tom = createSprite(870, 600);
+    tom.addImage("tomSleeping", tomImg1);
+    tom.scale = 0.2;
+
+    jerry = createSprite(200, 600);
+    jerry.addImage("jerryStanding", jerryImg1);
+    jerry.scale = 0.15;
+
+}
+
+function draw() {
+
+    background(bg);
+
+    if(tom.x - jerry.x < (tom.width - jerry.width)/2)
+    { 
+        tom.velocityX=0;
+        tom.addImage("tomLastImage", tomImg3);
+        tom.x =300;
+        tom.scale=0.2;
+        tom.changeAnimation("tomLastImage", tomImg2);
+        jerry.addImage("jerryLastImage", jerryImg3);
+        jerry.scale=0.15;
+        jerry.changeAnimation("jerryLastImage", jerryImg2);
+    }  
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+    if(keyCode === LEFT_ARROW){
+        tom.velocityX = -5; 
+        tom.addAnimation("tomRunning", tomImg2);
+        tom.changeAnimation("tomRunning");
+        
+        jerry.addAnimation("jerryTeasing", jerryImg2);
+        jerry.frameDelay = 25;
+        jerry.changeAnimation("jerryTeasing");
+    }
+}
